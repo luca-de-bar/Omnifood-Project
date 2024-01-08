a69ae1c539f51649dc565371e0227c1ae962f787
Koteyo <lucadebar33@gmail.com>
2024-01-08 18:14:15 +0100

    navbar-hamburger

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 window.addEventListener('scroll', () => {
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 50) { // Cambia '50' con il numero di pixel dopo cui vuoi che cambi lo stile
@@ -6,3 +8,9 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('scrolled');
     }
 });
+
+
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.querySelector('.menu-list').classList.toggle('show');
+  });
+  
